624b7988cbecff63ff216766a29bcec9c84302a7
leandro <oleandro.ti@gmail.com>
2017-07-17 16:53:40 -0300

    remoção

--- app.js
+++
@@ -1,7 +0,0 @@
-let app = require('./config/express')();
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
-
-app.set('io', io);
-
-http.listen(3000, () => console.log("Servidor Rodando"));
